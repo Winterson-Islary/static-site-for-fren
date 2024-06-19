4fa3be638530639ccff21016b15434175de55e72
Winterson <u19cse1028@cit.ac.in>
2024-06-19 22:41:47 +0530

    initial configurations

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import "@/styles/index.css";
 
 const Root: HTMLElement = document.getElementById("root") as HTMLElement;
 ReactDOM.createRoot(Root).render(
