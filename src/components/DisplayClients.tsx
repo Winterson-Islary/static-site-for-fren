3730047964ae709f0b1cfc16d52be674bd95a409
Winterson <u19cse1028@cit.ac.in>
2024-06-24 18:43:36 +0530

    added component to help display clients

---
+++ src/components/DisplayClients.tsx
@@ -0,0 +1,19 @@
+function DisplayClients() {
+	const clientImages = ["img1", "img2", "img3", "img4", "img5"];
+	return (
+		<section className="flex items-center gap-2 justify-evenly mx-auto w-[70%] flex-wrap pointer-events-none">
+			{clientImages.map((image) => {
+				return (
+					<img
+						className=" h-[50px] w-[125px] object-contain px-1"
+						src={`./src/assets/clients/${image}.webp`}
+						alt="client-image"
+						key={image}
+					/>
+				);
+			})}
+		</section>
+	);
+}
+
+export default DisplayClients;
