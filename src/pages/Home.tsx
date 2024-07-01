dcb08aa319a631f140765f611bebb30672f74c7e
Winterson <u19cse1028@cit.ac.in>
2024-07-01 23:53:23 +0530

    added Our process section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import DisplayClients from "@/components/DisplayClients";
 import DisplayFeatures from "@/components/DisplayFeatures";
+import DisplayProcess from "@/components/DisplayProcess";
 import DisplayServices from "@/components/DisplayServices";
 import HeroImg from "@/components/HeroImg";
 
@@ -8,18 +9,18 @@ const Home = () => {
 		<>
 			<div
 				id="hero-section"
-				className="pt-[3rem] md:pt-[5rem] h-[55rem] md:h-screen"
+				className="pt-[3rem] md:pt-[5rem] h-[55rem] lg:h-screen"
 			>
 				<HeroImg />
-				<section className="flex flex-col text-white mx-auto h-full w-[80%] lg:w-[50%] justify-center text-center items-center">
-					<span className=" font-tanker font-bold text-5xl md:text-8xl">
+				<section className="flex flex-col text-white mx-auto h-full w-[80%] lg:w-[50%] justify-center text-left items-center">
+					<span className=" font-playfair font-bold text-5xl md:text-7xl xl:text-9xl">
 						Your Premier Professional{" "}
-						<span className="underline text-orange-500 font-playfair">
+						<span className="underline text-[#cbfa40] font-playfair">
 							ERRANDS
 						</span>{" "}
 						running service...
 					</span>
-					<span className="font-thin pt-5 md:text-2xl md:w-[70%] xl:w-[55%]">
+					<span className="font-thin pt-5 md:text-xl xl:text-2xl md:w-[70%] xl:w-[55%] text-right">
 						<p className="tracking-tight leading-snug text-background font-tanker">
 							<span className="bg-black">
 								Professional Errand Runners for All Your Needs : Finding,
@@ -32,12 +33,18 @@ const Home = () => {
 			</div>
 			<div
 				id="clients-container"
-				className=" min-h-[10rem] py-10 flex flex-col justify-center bg-gray-100 "
+				className=" min-h-[10rem] pb-10 flex flex-col justify-center bg-gray-100 "
 			>
-				<span className="font-playfair font-bold text-center text-5xl tracking-tight">
-					Clients.
-				</span>
-				<section className="pt-5">
+				<section className="flex flex-col justify-center w-full bg-black">
+					<span className="font-playfair font-bold text-center text-5xl tracking-tight text-white pt-10">
+						Clients.
+					</span>
+					<span className="font-playfair italic text-white text-lg font-light text-center pb-3">
+						<strong>" </strong>We are proud to work with our exceptional
+						clients!<strong> "</strong>
+					</span>
+				</section>
+				<section className="pt-3">
 					<DisplayClients />
 				</section>
 			</div>
@@ -63,7 +70,7 @@ const Home = () => {
 				className=" h-full w-full mx-auto bg-white flex flex-col xl:flex-row border-y-[0.05rem] border-black"
 			>
 				<section className="flex flex-col w-full xl:w-[45%] bg-black justify-center text-white py-10 md:px-1">
-					<span className="font-playfair tracking-tight text-5xl md:text-7xl font-bold text-center">
+					<span className="font-playfair tracking-tight text-5xl md:text-7xl font-bold text-center ">
 						What we do.
 					</span>
 					<span className="text-white text-center pt-2 font-playfair font-light text-lg italic">
@@ -75,12 +82,22 @@ const Home = () => {
 					<DisplayServices />
 				</section>
 			</div>
-
 			<div
-				id="place-holder"
-				className=" h-screen text-center text-2xl font-bold overflow-hidden"
+				id="process-holder"
+				className=" h-full overflow-hidden py-20 md:py-[10rem]"
 			>
-				{/* <DisplayServiceImage /> */}
+				<section className="text-center flex flex-col">
+					<span className="text-5xl md:text-7xl font-playfair font-bold tracking-tight">
+						Our Process.
+					</span>
+					<span className="font-playfair text-center italic text-lg pt-2">
+						<strong>" </strong>Life made easy in 3 simple steps.
+						<strong> "</strong>
+					</span>
+				</section>
+				<section className="pt-5">
+					<DisplayProcess />
+				</section>
 			</div>
 		</>
 	);
