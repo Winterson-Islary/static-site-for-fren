ac6eaa051626e7bf70807e4a5a434cc6af716920
Winterson <u19cse1028@cit.ac.in>
2024-07-05 22:50:42 +0530

    layout changes in process section

--- src/components/DisplayProcess.tsx
+++ src/components/DisplayProcess.tsx
@@ -1,52 +1,56 @@
 function DisplayProcess() {
 	return (
-		<div className="grid grid-cols-1 lg:grid-cols-2 w-[95%] md:w-[50%] mx-auto gap-2">
-			<section className="">
+		<div className="flex flex-col w-[95%] lg:w-[50%] 2xl:w-[35%] mx-auto gap-11">
+			<section className="flex flex-col sm:flex-row  gap-6 items-center sm:items-start">
 				<img
-					className="w-full object-cover"
+					className="w-[200px] h-[150px] rounded-lg object-cover"
 					src="./src/assets/img2.webp"
 					alt="step 1 of process"
 				/>
-				<section className="flex flex-col justify-end pt-5 pb-2 font-playfair">
+				<section className="flex flex-col justify-center pt-5 pb-2 font-playfair text-center sm:text-left">
 					<span>
 						STEP <span className="font-montserrat">1.</span>
 					</span>
-					<span className="font-semibold text-xl">Reach us on WhatsApp.</span>
+					<span className="font-semibold text-xl uppercase">
+						Reach us on WhatsApp.
+					</span>
 					<span>
 						Connect directly with your dedicated virtual assistant via WhatsApp
 						for personalized, one-on-one assistance.
 					</span>
 				</section>
 			</section>
-			<section className="">
+			<section className="flex flex-col sm:flex-row-reverse gap-6 items-center sm:items-start">
 				<img
-					className="w-full object-cover"
+					className="w-[200px] h-[150px] object-cover rounded-lg"
 					src="./src/assets/img2.webp"
 					alt="step 1 of process"
 				/>
-				<section className="flex flex-col justify-end pt-5 pb-2 font-playfair">
+				<section className="flex flex-col justify-center text-center sm:text-right pt-5 pb-2 font-playfair">
 					<span>
 						STEP <span className="font-montserrat">2.</span>
 					</span>
-					<span className="font-semibold text-xl">We get to work.</span>
-					<span>
+					<span className="font-semibold text-xl uppercase">
+						We get to work.
+					</span>
+					<span className="">
 						The moment you reach out, we spring into action! Our dedicated team
 						will promptly begin working to find the ideal solution for your
 						specific needs.
 					</span>
 				</section>
 			</section>
-			<section className=" lg:col-span-2 flex flex-col lg:flex-row">
+			<section className="flex flex-col sm:flex-row  gap-6 items-center sm:items-start">
 				<img
-					className="w-full lg:w-[50%] object-cover"
+					className="w-[200px] h-[150px] object-cover rounded-lg"
 					src="./src/assets/img2.webp"
 					alt="step 1 of process"
 				/>
-				<section className="flex flex-col justify-end py-2 lg:py-0 px-2 font-playfair">
+				<section className="flex flex-col justify-center text-center sm:text-left py-2 lg:py-0 px-2 font-playfair">
 					<span className="">
 						STEP <span className="font-montserrat">3.</span>{" "}
 					</span>
-					<span className="text-xl font-semibold">
+					<span className="text-xl font-semibold uppercase">
 						Your guaranteed satisfaction.
 					</span>
 					<span>
