690a046cdeb87ca08c9570e70883fd3070564023
Winterson <u19cse1028@cit.ac.in>
2024-07-07 22:47:11 +0530

    change layout and img src

--- src/components/DisplayFeatures.tsx
+++ src/components/DisplayFeatures.tsx
@@ -2,28 +2,28 @@ function DisplayFeatures() {
 	const featureList = [
 		{
 			icon: "img2",
-			title: "Find",
+			title: "Find.",
 			desc: "Our service helps you find exactly what you need, quickly and efficiently.",
 			color: "#af9159",
 			number: 1,
 		},
 		{
 			icon: "img2",
-			title: "Book",
+			title: "Book.",
 			desc: "Easily book appointments and reservations with our streamlined, hassle-free process.",
 			color: "#efe9dd",
 			number: 2,
 		},
 		{
 			icon: "img2",
-			title: "Buy",
+			title: "Buy.",
 			desc: "Purchase items seamlessly with our reliable and secure buying service.",
 			color: "#af9159",
 			number: 3,
 		},
 		{
 			icon: "img2",
-			title: "Delivery",
+			title: "Delivery.",
 			desc: "Get your items delivered promptly and safely right to your doorstep.",
 			color: "#efe9dd",
 			number: 4,
@@ -37,21 +37,25 @@ function DisplayFeatures() {
 						key={feature.title}
 						// style={{ backgroundColor: `${feature.color}` }}
 						className={
-							" flex flex-col flex-1 p-3 gap-4 md:gap-0 items-center justify-around md:items-start min-w-[13em] md:min-h-[25rem] "
+							" flex flex-col flex-1 p-3 gap-4 md:gap-0 items-center justify-around md:items-start min-w-[13em] md:min-h-[25rem] shadow-md hover:shadow-xl ease-in-out duration-300 "
 						}
 					>
 						<img
-							className="h-[200px] w-full object-cover rounded-lg"
-							src={`./src/assets/${feature.icon}.webp`}
+							className="h-[200px] w-full object-cover rounded-lg shadow-md"
+							src={`/assets/${feature.icon}.webp`}
 							alt={`feature ${feature.title}`}
 							key={feature.title}
 						/>
 						<div className="flex flex-col gap-2 items-start  w-full">
 							<section className="text-left font-playfair flex flex-col gap-2">
-								<span className=" text-sm">[{feature.number}]</span>
-								<span className="text-3xl md:text-4xl">{feature.title}</span>
+								<span className=" text-sm font-semibold">
+									[{feature.number}]
+								</span>
+								<span className="text-3xl md:text-4xl font-black">
+									{feature.title}
+								</span>
 							</section>
-							<section className="font-playfair ">
+							<section className="font-playfair font-semibold ">
 								<p className="leading-tight tracking-tighter">{feature.desc}</p>
 							</section>
 						</div>
