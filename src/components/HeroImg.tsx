0caafa6124eb451cdb406414327099a639d313e4
Winterson <u19cse1028@cit.ac.in>
2024-07-07 22:47:48 +0530

    removed bg image and set solid color

--- src/components/HeroImg.tsx
+++ src/components/HeroImg.tsx
@@ -2,7 +2,6 @@ function HeroImg() {
 	return (
 		<section
 			id="hero-img-container"
-			// className=" -top-0 h-[75%] md:h-full w-full absolute  bg-[url('./src/assets/hero-section.webp')] brightness-[40%] bg-cover xl:bg-[center_top_-10rem] -z-50 pointer-events-none"
 			className=" -top-0 h-[75%] md:h-full w-full absolute bg-black  -z-50 pointer-events-none"
 		/>
 	);
