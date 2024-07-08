37d26b7c11987f91be2007db866bfb56d65e0206
Winterson <u19cse1028@cit.ac.in>
2024-07-09 03:56:09 +0530

    added message us component

---
+++ src/components/MessageUs.tsx
@@ -0,0 +1,20 @@
+function MessageUs() {
+	return (
+		<a href="/">
+			<div className="w-full flex">
+				<div className="bg-white flex items-center w-[200px] md:w-[250px] rounded-lg p-2 justify-center gap-2 hover:bg-[#cc3115]  hover:cursor-pointer  duration-300 ease-in-out select-none">
+					<img
+						className="w-[30px] md:w-[40px]"
+						src="/assets/whatsapp.png"
+						alt="send us a message"
+					/>
+					<span className="text-black font-tanker text-xl md:text-2xl">
+						Hire a runner
+					</span>
+				</div>
+			</div>
+		</a>
+	);
+}
+
+export default MessageUs;
