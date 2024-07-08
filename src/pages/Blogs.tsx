76b573f9619d103e9f86b8a6f3c62cbc60695b6a
Winterson <u19cse1028@cit.ac.in>
2024-07-08 23:50:42 +0530

    completed blogs page, indicated unavailable blogs

--- src/pages/Blogs.tsx
+++ src/pages/Blogs.tsx
@@ -1,8 +1,22 @@
 function Blogs() {
 	return (
-		<div className="h-screen pt-[3rem] md:pt-[5rem] bg-black">
-			<section>
-				<h1>Blogs</h1>
+		<div className="h-full md:h-screen py-[10rem]">
+			<section className=" flex flex-col gap-2 md:gap-5 items-center justify-center h-full">
+				<section className="relative">
+					<img
+						className="w-[500px] md:w-[700px] h-[500px] md:h-[700px]"
+						src="/assets/no-blogs.svg"
+						alt="no blogs illustration"
+					/>
+					<span className="text-slate-400 italic flex items-end justify-end -rotate-90 absolute bottom-[50%] -right-[5rem]">
+						<a href="https://storyset.com/people">
+							People illustrations by Storyset
+						</a>
+					</span>
+				</section>
+				<h1 className=" font-playfair text-3xl md:text-5xl text-center">
+					No Blogs Available Yet...
+				</h1>
 			</section>
 		</div>
 	);
