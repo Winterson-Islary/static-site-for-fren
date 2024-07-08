f3be5f149434a5a7c645003f7614cd1305654b07
Winterson <u19cse1028@cit.ac.in>
2024-07-08 23:48:34 +0530

    added links to pages and removed few footer items

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,25 +13,19 @@ function Footer() {
 							CONTACT US
 						</Link>
 
-						<Link to="/blogs" className="hover:underline">
-							BLOGS
-						</Link>
-						<Link to="" className="hover:underline">
+						<Link to="/terms" className="hover:underline">
 							TERMS OF SERVICES
 						</Link>
-						<Link to="" className="hover:underline">
+						<Link to="/terms" className="hover:underline">
 							PRIVACY POLICY
 						</Link>
 					</section>
 					<section className="text-white font-roboto text-3xl flex flex-col gap-2 my-10 font-bold text-nowrap">
-						<Link to="" className="hover:underline">
-							PERSONALIZED SERVICES
+						<Link to="/services" className="hover:underline">
+							SERVICES
 						</Link>
-						<Link to="" className="hover:underline">
-							BUSINESS SERVICES
-						</Link>
-						<Link to="" className="hover:underline">
-							DELIVERY SERVICES
+						<Link to="/blogs" className="hover:underline">
+							BLOGS
 						</Link>
 					</section>
 				</section>
@@ -61,14 +55,14 @@ function Footer() {
 			<div className="w-[80%]  mx-auto border-t-4 border-white py-10">
 				<section className="flex justify-between items-center text-white pb-10">
 					<span className=" font-montserrat ">
-						2024 © WINTERSON, ALL RIGHTS RESERVED
+						2024 © GO-EAZY, ALL RIGHTS RESERVED
 					</span>
 				</section>
-				<section className="font-roboto font-medium text-md text-white text-center mx-auto w-[70%]">
-					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore,
-					provident. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-					Exercitationem animi nemo praesentium quod vitae. Vitae aliquid non
-					saepe, fugiat ut reprehenderit minima in ipsa dolor corporis
+				<section className="font-roboto font-medium text-md text-white text-center mx-auto md:w-[70%]">
+					Go-Eazy, its services and its websites, www.Go-Eazy.com, is owned
+					wholly by Go-Eazy Limited, Therefore, any referral to Go-Eazy or its
+					services, directly refers to Go-Eazy Limited. United Arab of Emirates
+					is our country of domicile
 				</section>
 			</div>
 		</footer>
