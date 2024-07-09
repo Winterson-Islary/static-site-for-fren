a249c5d3e76abe2648e8cc44d37606de2b3ee616
Winterson <u19cse1028@cit.ac.in>
2024-07-10 02:17:29 +0530

    added external link redirect to message us button

--- src/components/MessageUs.tsx
+++ src/components/MessageUs.tsx
@@ -1,6 +1,11 @@
 function MessageUs() {
 	return (
-		<a href="/">
+		<a
+			href="https://wa.me/971508211455"
+			target="_blank"
+			rel="noreferrer"
+			className="w-[200px] md:w-[250px]"
+		>
 			<div className="w-full flex">
 				<div className="bg-white flex items-center w-[200px] md:w-[250px] rounded-lg p-2 justify-center gap-2 hover:bg-[#cc3115]  hover:cursor-pointer  duration-300 ease-in-out select-none">
 					<img
