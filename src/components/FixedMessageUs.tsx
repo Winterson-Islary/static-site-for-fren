84dd67608a7499ba89cd3793257d249a0802a2cf
Winterson <u19cse1028@cit.ac.in>
2024-07-12 01:07:41 +0530

    added a fixed whatsapp logo for clients to request service

---
+++ src/components/FixedMessageUs.tsx
@@ -0,0 +1,11 @@
+function FixedMessageUs() {
+	return (
+		<a href="https://wa.me/971508211455" target="_blank" rel="noreferrer">
+			<div className="block h-[70px] w-[70px] md:h-[80px] md:w-[80px] fixed bottom-2 right-2 md:bottom-10 md:right-5  overflow-hidden  hover:cursor-pointer hover:scale-105 duration-300 ease-in-out select-none z-20">
+				<img src="/assets/whatsappFixed.png" alt="whatsapp logo" />
+			</div>
+		</a>
+	);
+}
+
+export default FixedMessageUs;
