49bdefe64ac06e9c820b34561d9f02719968eff0
Winterson <u19cse1028@cit.ac.in>
2024-07-19 00:16:02 +0530

    add on route change scroll to top

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import Lenis from "lenis";
 import { useEffect } from "react";
 import { Navbar } from "@/components/Navbar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "@/components/Footer.tsx";
 // import ScrollToTop from "./components/ScrollToTop";
 import FixedMessageUs from "./components/FixedMessageUs";
 
 function App() {
+	const { pathname } = useLocation();
 	useEffect(() => {
+		window.scrollTo(0, 0);
 		const lenis = new Lenis();
 		function raf(time: number) {
 			lenis.raf(time);
 			requestAnimationFrame(raf);
 		}
 		requestAnimationFrame(raf);
-	}, []);
+	}, [pathname]);
 
 	return (
 		<>
